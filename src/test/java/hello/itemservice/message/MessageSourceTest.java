4798aa9078ad6b05484c7ec994a0f817ee21ceed
HyebinPark96 <bini2718@naver.com>
2023-01-12 22:43:43 +0900

    국제화 검증 테스트코드 작성

---
+++ src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -0,0 +1,56 @@
+package hello.itemservice.message;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
+
+import java.util.Locale;
+
+import static org.assertj.core.api.Assertions.*;
+
+@SpringBootTest
+public class MessageSourceTest {
+
+    @Autowired
+    MessageSource ms; // application.properties에 지정된 messages 관련 .properties 가져옴
+
+    @Test
+    void helloMessage() {
+        String result = ms.getMessage("hello", null, null); // locale=null이므로, messages.properties 선택
+        assertThat(result).isEqualTo("안녕");
+    }
+
+    // 메시지가 없는 경우, 기본 메시지
+    @Test
+    void notFoundMessageCode() {
+        assertThatThrownBy(()->ms.getMessage("no_code", null, null))
+                .isInstanceOf(NoSuchMessageException.class);
+    }
+
+    @Test
+    void notFoundMessageCodeDefaultMessage() {
+        String result = ms.getMessage("no_code", null, "기본 메시지", null);
+        assertThat(result).isEqualTo("기본 메시지");
+    }
+
+    @Test
+    void argumentMessage() {
+        String message = ms.getMessage("hello.name", new Object[]{"Spring"}, null);
+        assertThat(message).isEqualTo("안녕 Spring");
+    }
+
+    // 국제화
+    @Test
+    void defaultLang() {
+        assertThat(ms.getMessage("hello", null, null)).isEqualTo("안녕");
+        assertThat(ms.getMessage("hello", null, Locale.KOREA)).isEqualTo("안녕");
+    }
+
+    @Test
+    void enLang() {
+        assertThat(ms.getMessage("hello", null, Locale.ENGLISH)).isEqualTo("hello");
+    }
+}
